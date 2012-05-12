968b47c24f43d61ee010800f856e51d9e22625e7
Takanashi Ginpei <ginpei@gmail.com>
2012-05-13 05:21:05 +0900

    aboutページを作製

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,4 +3,7 @@ class WelcomeController < ApplicationController
     @theme = Theme.active.first
     render 'themes/show'
   end
+
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Iemon::Application.routes.draw do
   get "/profile" => "user#edit"
   put "/profile" => "user#update"
 
+  get "/about" => "welcome#about"
+
   match "/auth/:provider/callback" => "sessions#callback"
   match "/auth/failure" => "sessions#failure"
   match "/logout" => "sessions#destroy", :as => :logout
