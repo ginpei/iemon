d05e72471ccf45c20e779d3a5a26fcba7156787d
Takanashi Ginpei <ginpei@gmail.com>
2012-05-13 01:41:47 +0900

    TwitterのSecret Keyをgit管理外のファイルへ分割
    ファイル名は
     * config/initializers/local_omniauth.rb
    にしてください。
    （このファイルをコミットしないように設定しました。）

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,3 @@
-ENV['TWITTER_KEY'] = "VGPObUV3PWCC5RYU2lQmWw"
-ENV['TWITTER_SECRET'] = "MqOE6OlZaUDU2erUPHZtBAgpo8SN83S6Ne1BL5fFqU"
-
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
