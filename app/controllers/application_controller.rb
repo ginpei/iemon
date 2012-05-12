cbf46825da92b07c5a287af95148cff2097532f9
dan5 <dan5.ya@gmail.com>
2012-05-12 20:42:37 +0900

    UnAuthorizedのときはauth/twitterへ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
 
   before_filter :reset_session_expires
 
+  rescue_from User::UnAuthorized, :with => -> { redirect_to '/auth/twitter' }
+
   private
 
   def reset_session_expires
